9ebb7e1f8e4d3c89405224acb4fad843010250ed
AkshatJha21 <akshatj2134@gmail.com>
2024-10-24 13:52:14 +0530

    base commit

---
+++ components/custom/Navbar.tsx
@@ -0,0 +1,12 @@
+
+
+const Navbar = () => {
+  return (
+    <div className="flex w-full py-4 px-6 items-center justify-between">
+        <h1 className="text-lg font-semibold">Nexpense</h1>
+        <div>User</div>
+    </div>
+  )
+}
+
+export default Navbar
