c12249739ee936d3c04f7a388c279a906f23f455
AkshatJha21 <akshatj2134@gmail.com>
2024-10-25 19:46:27 +0530

    navbar and bottom bar done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import BottomNav from "@/components/custom/BottomNav";
+import DashboardGrid from "@/components/custom/DashboardGrid";
 import Navbar from "@/components/custom/TopBar";
 
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen relative">
       <Navbar />
+      <DashboardGrid />
       <BottomNav />
     </div>
   );
---
+++ components/custom/DashboardGrid.tsx
@@ -0,0 +1,16 @@
+
+
+const DashboardGrid = () => {
+  return (
+    <div className="px-6 py-4 flex justify-between min-h-[90%]">
+        <div className="flex flex-col">
+            <h1>Welcome, Akshat</h1>
+        </div>
+        <div>
+            Right
+        </div>
+    </div>
+  )
+}
+
+export default DashboardGrid
