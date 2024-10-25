6fb7ab7075700bca3d7fd853d89351ef2eb2c0b3
AkshatJha21 <akshatj2134@gmail.com>
2024-10-25 09:25:58 +0530

    color scheme update

--- components/custom/BottomNav.tsx
+++ components/custom/BottomNav.tsx
@@ -2,7 +2,7 @@
 
 import { Dispatch, SetStateAction, useState } from "react";
 import { motion } from "framer-motion";
-import { CreditCard, DollarSign, Home, TrendingUp, Wallet } from "lucide-react";
+import { CreditCard, DollarSign, Home, TrendingUp } from "lucide-react";
 
 const tabs = ["Dashboard", "Expenses", "Income", "Credit"];
 
@@ -10,7 +10,7 @@ const BottomNav = () => {
     const [selected, setSelected] = useState(tabs[0]);
   return (
     <div className='fixed bottom-6 flex w-full items-center justify-center'>
-        <div className='flex gap-x-4 p-2 bg-black/90 dark:bg-white/10 rounded-md'>
+        <div className='flex gap-x-4 p-2 bg-gradient-to-b from-slate-100 to-slate-200 dark:from-white/10 dark:to-white/15  rounded-md w-[80%] sm:w-[60%] shadow-md justify-between'>
             {tabs.map((tab) => (
                 <Tab 
                     text={tab}
@@ -49,10 +49,10 @@ const Tab = ({ text, selected, setSelected }: TabProps) => {
             className={`${
                 selected 
                 ? "text-white"
-                : "text-slate-300 hover:text-slate-200 hover:bg-slate-700"  
-            } text-sm transition-colors px-4 py-2 rounded-md relative`}
+                : "text-slate-900 dark:text-slate-200 hover:bg-slate-300 dark:hover:bg-black/30"  
+            } text-sm transition-colors px-4 py-2 rounded-md relative w-1/6 md:w-1/4`}
         >
-            <div className="relative z-10 flex flex-col items-center">
+            <div className="relative z-10 flex flex-col items-center gap-y-1">
                 {getIcon()}
                 <span className="hidden md:flex font-medium">{text}</span>
             </div>
@@ -60,7 +60,7 @@ const Tab = ({ text, selected, setSelected }: TabProps) => {
                 <motion.span
                     layoutId="pill-tab"
                     transition={{ type: "spring", duration: 0.5 }}
-                    className="absolute inset-0 z-0 bg-indigo-400 rounded-md"
+                    className="absolute inset-0 z-0 bg-gradient-to-tr from-indigo-700 to-[#d6ade0] dark:from-indigo-700 dark:to-[#d6ade0] rounded-md"
                 >
                 </motion.span>
             )}
--- components/ui/UserButton.tsx
+++ components/ui/UserButton.tsx
@@ -33,7 +33,7 @@ const UserButton = () => {
   return (
     <div className="relative flex items-center gap-x-6">
         <p>Hi 👋🏻, Akshat</p>
-        <div ref={buttonRef} onClick={openMenu} className="relative h-10 w-10 rounded-full transition bg-gradient-to-b from-indigo-600 to-indigo-900 hover:text-opacity-50 cursor-pointer text-white flex items-center justify-center">
+        <div ref={buttonRef} onClick={openMenu} className="relative h-10 w-10 rounded-full transition bg-gradient-to-tr from-indigo-700 to-[#d6ade0] hover:text-opacity-50 cursor-pointer text-white flex items-center justify-center">
             <User className="h-5"/>
         </div>
         <div className="absolute top-14 right-0" ref={dropdownRef}>
@@ -50,18 +50,18 @@ const UserButton = () => {
 const Dropdown = () => {
     return (
         <motion.div 
-            className=" border dark:border-none rounded bg-gradient-to-b dark:from-indigo-600 from-slate-100 to-slate-200 shadow-sm dark:to-indigo-900 p-2 dark:text-white text-black"
+            className=" border dark:border-none rounded bg-gradient-to-b dark:from-white/10 dark:to-white/15 from-slate-100 to-slate-200 shadow-sm p-2 dark:text-slate-200 text-black"
             initial={{ opacity: 0, y: -10 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -10 }}
             transition={{ duration: 0.25 }}
         >
             <div className="flex flex-col items-start gap-y-1 text-sm">
-                <button className="py-2 px-4 transition-all hover:bg-slate-300 dark:hover:bg-opacity-10 rounded flex w-full items-center">
+                <button className="py-2 px-4 transition-all hover:bg-black/30 dark:hover:bg-opacity-10 rounded flex w-full items-center">
                     <Settings className="h-5 mr-2"/>
                     <p className="font-medium">Settings</p>
                 </button>
-                <button className="py-2 px-4 transition-all hover:bg-slate-300 dark:hover:bg-opacity-10 rounded flex w-full items-center">
+                <button className="py-2 px-4 transition-all hover:bg-black/30 dark:hover:bg-opacity-10 rounded flex w-full items-center">
                     <LogOut className="h-5 mr-2"/>
                     <p className="font-medium">Logout</p>
                 </button>
