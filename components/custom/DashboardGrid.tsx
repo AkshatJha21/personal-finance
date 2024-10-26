95f1da56872e73495ee1d9aaa94839c41d4cde25
AkshatJha21 <akshatj2134@gmail.com>
2024-10-26 12:56:18 +0530

    monthly grid done

--- components/custom/DashboardGrid.tsx
+++ components/custom/DashboardGrid.tsx
@@ -1,4 +1,4 @@
-import { ArrowBigRight, ArrowLeftRight, Link, Link2Off, MoreHorizontal, PlusIcon, Send, SquareArrowRight } from "lucide-react"
+import { ArrowBigRight, ArrowDownCircle, ArrowLeftRight, ArrowUpCircle, Link, Link2Off, MoreHorizontal, PlusIcon, Send, SquareArrowRight } from "lucide-react"
 
 const DashboardGrid = () => {
   return (
@@ -12,6 +12,38 @@ const DashboardGrid = () => {
                     <PlusIcon className="h-4"/>
                     <p className="text-sm font-medium">New Transaction</p>
                 </button>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-3 py-8 gap-6">
+                <div className="flex flex-col gap-y-8 border rounded-2xl p-6 dark:border-neutral-500 bg-gradient-to-t dark:bg-gradient-to-b from-indigo-100 to-slate-50 dark:from-neutral-950 dark:to-white/5">
+                    <p className="text-sm font-light">Monthly Income</p>
+                    <div className="flex items-baseline justify-between">
+                        <p className="text-3xl font-medium tracking-wide">$ 5347.68</p>
+                        <div className="flex items-center text-rose-500">
+                            <ArrowDownCircle className="h-3"/>
+                            <p className="text-xs font-semibold">20%</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-y-8 border rounded-2xl p-6 dark:border-neutral-500 bg-gradient-to-t dark:bg-gradient-to-b from-indigo-100 to-slate-50 dark:from-neutral-950 dark:to-white/5">
+                    <p className="text-sm font-light">Monthly Expense</p>
+                    <div className="flex items-baseline justify-between">
+                        <p className="text-3xl font-medium tracking-wide">$ 3403.45</p>
+                        <div className="flex items-center text-green-500">
+                            <ArrowUpCircle className="h-3"/>
+                            <p className="text-xs font-semibold">10%</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-y-8 border rounded-2xl p-6 dark:border-neutral-500 bg-gradient-to-t dark:bg-gradient-to-b from-indigo-100 to-slate-50 dark:from-neutral-950 dark:to-white/5">
+                    <p className="text-sm font-light">Monthly Savings</p>
+                    <div className="flex items-baseline justify-between">
+                        <p className="text-3xl font-medium tracking-wide">$ 1944.23</p>
+                        <div className="flex items-center text-rose-500">
+                            <ArrowDownCircle className="h-3"/>
+                            <p className="text-xs font-semibold">15%</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <div className="flex flex-col w-full md:w-[30%] items-center h-[85vh] p-8 border md:ml-6 rounded-2xl dark:border-neutral-500 bg-gradient-to-t dark:bg-gradient-to-b from-indigo-100 to-slate-50 dark:from-neutral-950 dark:to-white/5">
