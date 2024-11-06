8ba35790698b29db9b993201277fccafbaf29d2e
AkshatJha21 <akshatj2134@gmail.com>
2024-11-06 11:25:14 +0530

    chart js added

--- components/custom/DashboardGrid.tsx
+++ components/custom/DashboardGrid.tsx
@@ -1,6 +1,37 @@
 "use client";
 
-import { ArrowDownCircle, ArrowLeftRight, ArrowUpCircle, MoreHorizontal, PlusIcon, SquareArrowRight } from "lucide-react"
+import { ArrowDownCircle, ArrowLeftRight, ArrowUpCircle, MoreHorizontal, PlusIcon, SquareArrowRight } from "lucide-react";
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, BarElement, Title, Tooltip, Legend, Filler, } from "chart.js";
+import dynamic from 'next/dynamic';
+import { Bar } from "react-chartjs-2";
+ChartJS.register(CategoryScale, LinearScale, PointElement, BarElement, Title, Tooltip, Legend, Filler );
+
+const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: 'GeeksforGeeks Bar Chart',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
 const DashboardGrid = () => {
   return (
@@ -110,7 +141,10 @@ const DashboardGrid = () => {
                     <p className="text-xl font-semibold">Spending Analytics</p>
                     <button className="text-xs font-medium dark:text-pink-400 text-pink-500 hover:underline">View All</button>
                 </div>
-                Chart JS
+                <div style={{ width: '700px' }}>
+                    <h1>Example 2: Bar Chart</h1>
+                    <Bar data={data} />
+                </div>
             </div>
         </div>
         <div className="flex flex-col w-full md:w-[30%] items-center h-[85vh] p-8 border md:ml-6 rounded-2xl dark:border-neutral-500 bg-gradient-to-t dark:bg-gradient-to-b from-indigo-100 to-slate-50 dark:from-neutral-950 dark:to-white/5">
