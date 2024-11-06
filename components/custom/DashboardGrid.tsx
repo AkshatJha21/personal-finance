b9c7528fdc2900ee30309ba563fab4021fec9d72
AkshatJha21 <akshatj2134@gmail.com>
2024-11-06 12:10:27 +0530

    pie chart color scheme

--- components/custom/DashboardGrid.tsx
+++ components/custom/DashboardGrid.tsx
@@ -15,9 +15,9 @@ const data = {
       label: '$',
       data: [5347, 3403, 1944],
       backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
+        'rgb(75, 60, 193)',
+        'rgb(157, 89, 239)',
+        'rgb(220, 118, 180)'
       ],
       hoverOffset: 4
     }]
