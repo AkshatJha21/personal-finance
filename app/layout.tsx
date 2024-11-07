79ff3bf6b58e57d51af318475be7e333d93c65ab
AkshatJha21 <akshatj2134@gmail.com>
2024-11-07 11:35:52 +0530

    7/11

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const monsterrat = Montserrat({
+const montserrat = Montserrat({
   weight: ['200', '300', '400', '500', '600', '700' , '800'],
   subsets: ['latin']
 })
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={monsterrat.className}
+        className={montserrat.className}
       >
         {children}
       </body>
