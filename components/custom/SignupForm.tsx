ac078265242f8a537ef7383960a3bfccc2f3c4c5
AkshatJha21 <akshatj2134@gmail.com>
2024-11-10 12:46:14 +0530

    auth page design complete

--- components/custom/SignupForm.tsx
+++ components/custom/SignupForm.tsx
@@ -20,13 +20,13 @@ const SignupForm = () => {
       {/* Progress Bar */}
       <div className="w-[80%] sm:w-[40%] lg:w-[30%] xl:w-[25%] mb-4">
         <div className="flex justify-between text-sm mb-1">
-          <span className={step >= 1 ? 'text-indigo-600' : 'text-gray-400'}>Step 1</span>
-          <span className={step >= 2 ? 'text-indigo-600' : 'text-gray-400'}>Step 2</span>
-          <span className={step >= 3 ? 'text-indigo-600' : 'text-gray-400'}>Step 3</span>
+          <span className={step >= 1 ? 'text-indigo-600 dark:text-indigo-400' : 'text-slate-400 dark:text-neutral-500'}>Step 1</span>
+          <span className={step >= 2 ? 'text-indigo-600 dark:text-indigo-400' : 'text-slate-400 dark:text-neutral-500'}>Step 2</span>
+          <span className={step >= 3 ? 'text-indigo-600 dark:text-indigo-400' : 'text-slate-400 dark:text-neutral-500'}>Step 3</span>
         </div>
-        <div className="relative h-1 w-full bg-gray-300">
+        <div className="relative h-1 w-full bg-slate-300 dark:bg-neutral-700">
           <div
-            className="absolute top-0 h-full bg-indigo-600 transition-all duration-300"
+            className="absolute top-0 h-full bg-indigo-600 dark:bg-indigo-400 transition-all duration-300"
             style={{ width: `${(step / 3) * 100}%` }}
           />
         </div>
@@ -36,7 +36,10 @@ const SignupForm = () => {
       <div className="flex flex-col gap-y-6 w-[80%] sm:w-[40%] lg:w-[30%] xl:w-[25%]">
         {step === 1 && (
           <>
-            <h2 className="text-xl lg:text-2xl font-semibold">Step 1: Account Setup</h2>
+            <div>
+              <h2 className="text-xl lg:text-2xl font-semibold">Get Started</h2>
+              <p className='text-sm font-light'>Create a new account</p>
+            </div>
             <FormInput label="Email" type="email" handleChange={() => {}} />
             <FormInput label="New Password" type="password" handleChange={() => {}} />
             <FormInput label="Confirm Password" type="password" handleChange={() => {}} />
@@ -44,7 +47,10 @@ const SignupForm = () => {
         )}
         {step === 2 && (
           <>
-            <h2 className="text-xl lg:text-2xl font-semibold">Step 2: Personal Info</h2>
+            <div>
+              <h2 className="text-xl lg:text-2xl font-semibold">Personal Information</h2>
+              <p className='text-sm font-light'>Enter your detials to continue</p>
+            </div>
             <FormInput label="First Name" type="text" handleChange={() => {}} />
             <FormInput label="Last Name" type="text" handleChange={() => {}} />
             <FormInput label="Phone Number" type="text" handleChange={() => {}} />
@@ -52,10 +58,13 @@ const SignupForm = () => {
         )}
         {step === 3 && (
           <>
-            <h2 className="text-xl lg:text-2xl font-semibold">Step 3: Profile Details</h2>
+            <div>
+              <h2 className="text-xl lg:text-2xl font-semibold">Setup Account</h2>
+              <p className='text-sm font-light'>Fill payment detials to finish setup</p>
+            </div>
             <FormInput label="Account Number" type="text" handleChange={() => {}} />
-            <FormInput label="Set Nex-PIN" type="text" handleChange={() => {}} />
-            <FormInput label="Confirm Nex-PIN" type="text" handleChange={() => {}} />
+            <FormInput label="Set ePIN" type="text" handleChange={() => {}} />
+            <FormInput label="Confirm ePIN" type="text" handleChange={() => {}} />
           </>
         )}
 
@@ -63,7 +72,7 @@ const SignupForm = () => {
         <div className="flex justify-between">
           {step > 1 && (
             <button
-              className="px-4 py-2 font-medium rounded-md bg-gray-400 hover:bg-gray-500 text-white"
+              className="px-4 py-2 font-medium rounded-md bg-purple-200 hover:bg-indigo-200 dark:bg-white/10 dark:hover:bg-white/15 transition-all w-full mr-2"
               onClick={handlePreviousStep}
             >
               Previous
@@ -71,13 +80,13 @@ const SignupForm = () => {
           )}
           {step < 3 ? (
             <button
-              className="px-4 py-2 font-medium rounded-md bg-indigo-600 hover:bg-indigo-700 text-white"
+              className="px-4 py-2 font-medium rounded-md transition bg-gradient-to-tr from-indigo-700 to-[#d6ade0] hover:opacity-80 cursor-pointer text-white w-full"
               onClick={handleNextStep}
             >
               Next
             </button>
           ) : (
-            <button className="px-4 py-2 font-medium rounded-md bg-green-600 hover:bg-green-700 text-white">
+            <button className="px-4 py-2 font-medium rounded-md transition bg-gradient-to-tr from-indigo-700 to-[#d6ade0] hover:opacity-80 cursor-pointer text-white w-full">
               Submit
             </button>
           )}
