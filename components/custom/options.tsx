c4178cb74c9e70feb196cfd3aadff14e8d310a3b
AkshatJha21 <akshatj2134@gmail.com>
2024-11-17 07:06:04 +0530

    wallet modal on options

--- components/custom/options.tsx
+++ components/custom/options.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { ArrowLeftRight, MoreHorizontal, SquareArrowRight, Wallet } from "lucide-react"
+import { MoreHorizontal, SquareArrowRight, Wallet } from "lucide-react"
 import { useState } from "react";
 import PaymentModal from "./PaymentModal";
+import WalletModal from "./WalletModal";
 
 export const Options = () => {
     const [openPaymentModal, setOpenPaymentModal] = useState(false);
+    const [openWalletModal, setOpenWalletModal] = useState(false);
     return (
         <>
             <div className="flex w-full items-center h-[25%] justify-between gap-x-6 dark:text-slate-200">
@@ -13,7 +15,7 @@ export const Options = () => {
                     <SquareArrowRight className="h-4 sm:h-5"/>
                     <p className="text-xs font-medium">Pay</p>
                 </button>
-                <button className="bg-purple-200 hover:bg-indigo-200 dark:bg-white/10 dark:hover:bg-white/15 transition-all rounded-full flex flex-col items-center justify-center gap-y-2 aspect-square w-[30%]">
+                <button onClick={() => setOpenWalletModal(true)} className="bg-purple-200 hover:bg-indigo-200 dark:bg-white/10 dark:hover:bg-white/15 transition-all rounded-full flex flex-col items-center justify-center gap-y-2 aspect-square w-[30%]">
                     <Wallet className="h-4 sm:h-5"/>
                     <p className="text-xs font-medium">Wallet</p>
                 </button>
@@ -24,6 +26,9 @@ export const Options = () => {
             </div>
             {openPaymentModal && (
                 <PaymentModal onClose={() => setOpenPaymentModal(false)} />
+            )}
+            {openWalletModal && (
+                <WalletModal onClose={() => setOpenWalletModal(false)} />
             )}
         </>
     )
